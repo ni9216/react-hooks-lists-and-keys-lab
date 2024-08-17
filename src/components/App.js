e30dda3b18d9d3f6dc4238681f22a703381ca74e
ni9216 <nicholasliech851@gmail.com>
2024-08-17 12:30:04 +0300

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
-import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 import ProjectList from "./ProjectList";
-import user from "../data/user";
+import projectData from "../data/projects"; // Assuming this is where your project data is stored
+
+
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <Home />
+      <About />
+      <ProjectList projects={projectData} />
     </div>
   );
 }
---
+++ src/data/projects.js
@@ -0,0 +1,23 @@
+const projectData = [
+    {
+      id: 1,
+      name: "Project 1",
+      about: "This is the first project.",
+      technologies: ["React", "JavaScript", "CSS"]
+    },
+    {
+      id: 2,
+      name: "Project 2",
+      about: "This is the second project.",
+      technologies: ["HTML", "CSS", "JavaScript"]
+    },
+    {
+      id: 3,
+      name: "Project 3",
+      about: "This is the third project.",
+      technologies: ["Node.js", "Express", "MongoDB"]
+    }
+  ];
+  
+  export default projectData;
+  
